31f67617a69417f2fa939ffbdd2c01fcf6bdf952
eduartvlusha-a11y <eduartvlusha@gmail.com>
2026-03-26 19:40:19 +0100

    fix serverless nest

--- api/index.ts
+++ api/index.ts
@@ -1,24 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../src/app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import serverless from 'serverless-http';
 
-let app;
+let server;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('MealWise API')
+    .setDescription('MealWise Engine API Docs')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverless(expressApp);
+}
 
 export default async function handler(req, res) {
-  if (!app) {
-    app = await NestFactory.create(AppModule);
-
-    const config = new DocumentBuilder()
-      .setTitle('MealWise API')
-      .setDescription('MealWise Engine API Docs')
-      .setVersion('1.0')
-      .build();
-
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api/docs', app, document);
-
-    await app.init();
+  if (!server) {
+    server = await bootstrap();
   }
-
-  return app.getHttpAdapter().getInstance()(req, res);
+  return server(req, res);
 }
